5e21007eb9a0a8474f779cdb732660e2ac74508f
Alyaa Rana Raya <alyaaranaraya@gmail.com>
2024-01-01 08:54:54 +0700

    main banner

---
+++ src/components/Banner.js
@@ -0,0 +1,33 @@
+import { Col, Container,Row } from "react-bootstrap";
+import { BsArrowRight } from 'react-icons/bs';
+import headerImg from "../assets/img/header-img.svg";
+import { useEffect, useState } from "react";
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Frontend Deeveloper", "Data Scientist", "UI/UX Designer"];
+    const [text, setText] = useState ('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const period = 2000;
+
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portofolio</span>
+                        <h1>{'Hi, Im Alyaa'} Frontend Developer </h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <button onClick={() => console.log('connect')}>Let’s Connect <BsArrowRight size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img"/>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
