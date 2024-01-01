8dbca35734952d1fc96c2d6d65f16f6757c4fbc6
Alyaa Rana Raya <alyaaranaraya@gmail.com>
2024-01-01 09:23:42 +0700

    Skill Slider Carousel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {NavBar} from './components/NavBar.js';
 import {Banner} from './components/Banner.js';
+import {Skills} from './components/Skills.js';
 import 'bootstrap/dist/css/bootstrap.min.css';  // Fix the typo here
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
